1023c277c8315370c7fb5260b0c569175b623f96
Gena <106618614+GS364@users.noreply.github.com>
2025-04-30 19:58:50 +0300

    Add files via upload

--- tests/test_pokemons.py
+++ tests/test_pokemons.py
@@ -2,23 +2,27 @@ import requests
 import pytest
 
 URL = "https://api.pokemonbattle.ru/v2"
-TOKEN = "af5467b303a405945437695ff2d7c905"
+TOKEN = "USER_TOKEN"
 HEADER = {"Content-Type": "application/json", "trainer_token": TOKEN}
 TRAINER_ID = "28963"
 
-def test_status_code():
-    response = requests.get(url= f'{URL}/trainers', params={"trainer_id":TRAINER_ID})
-    assert response.status_code == 200 
 
-def test_trainers():
-    response = requests.get(url= f'{URL}/trainers')
+def test_status_code():
+    response = requests.get(url=f"{URL}/trainers", params={"trainer_id": TRAINER_ID})
     assert response.status_code == 200
 
-def my_trainer():
-    response = requests.get(url= f'{URL}/trainers', params= {"trainer_id":TRAINER_ID})
-    assert response.json()['trainer_name']==['Геназавр']
 
-'''@pytest. mark.parametrize('key, value', [('name', 'Бульбазавр'), ('trainer_id', TRAINER_ID), ('id', '279358')])
+def test_trainers():
+    response = requests.get(url=f"{URL}/trainers")
+    assert response.status_code == 200
+
+
+def my_trainer():
+    response = requests.get(url=f"{URL}/trainers", params={"trainer_id": TRAINER_ID})
+    assert response.json()["trainer_name"] == ["Геназавр"]
+
+
+"""@pytest. mark.parametrize('key, value', [('name', 'Бульбазавр'), ('trainer_id', TRAINER_ID), ('id', '279358')])
 def test_parametrize(key, value):
     response_parametrize = requests.get(url = f'{URL}/pokemons', params = {'trainer_id': TRAINER_ID})
-    assert response_parametrize.json()["data"][0][key] == value'''
+    assert response_parametrize.json()["data"][0][key] == value"""
